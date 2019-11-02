076eef92aca5a6f1efb6c8e78fd6c3d3c964fa37
Manuel <pfandlmanuel@protonmail.com>
2019-11-02 14:09:21 +0100

    finished scrapper

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
+import csv
 
 page_number_max = int(input("Number of Pages on the Website: "))
 page_number = 1
+
+filename = "index_exhibitors.csv"
+f = open(filename, "w")
+headers = "company_name; country; link"
+f.write(headers)
 
 for x in range(0, page_number_max):
     myurl = "https://guide.swissbau.ch/de/aussteller?page=" + str(page_number)
@@ -19,12 +25,15 @@ for x in range(0, page_number_max):
     #grabs each object with the no-image tag & image
     containers = page_soup.findAll("li", {"class":"ngn-content-box ngn-content-box-horizontal ngn-bookmark-button-visible"})
 
-    #loop container
+    #loop container to get infos and safe to csv
     for container in containers:
         #name of the company
-        name = container.div.picture.img["alt"]
+        company_name = container.div.picture.img["alt"]
         #country of company
         country_container = container.findAll("div", {"class":"ngn-search-card-country ngn-block"})
         country = country_container[0].text.strip()
         #link to profile page
         link = "https://guide.swissbau.ch" + container.div.a["href"]
+        f.write("\n" + company_name + ";" + country + ";" + link)
+
+f.close()
