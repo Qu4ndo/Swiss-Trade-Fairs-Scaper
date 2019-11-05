ad1d63b6700a117f44de80a58f57529e4c8b75fb
Manuel <pfandlmanuel@protonmail.com>
2019-11-05 19:24:55 +0100

    Finished

--- scraper.py
+++ scraper.py
@@ -3,19 +3,53 @@ from urllib.request import urlopen as uReq
 import csv
 
 
+#get the right url
+def url():
+    print("Here are the Fare to choose:")
+
+    #names of all fairs
+    fare_list = [
+    "SwissBau",
+    "Igeho"
+    ]
+
+    #url of all fares
+    url_list = [
+    "https://guide.swissbau.ch",
+    "https://guide.igeho.ch"
+    ]
+
+    list_number = 1
+
+    #loop print fare names
+    for fare in fare_list:
+        print(str(list_number) + " - " + fare)
+        list_number += 1
+
+    #choosing the right fare from lists
+    choice_fare = input("Please choose your fare: ")
+    choice_fare = int(choice_fare) - 1
+    fare_url = url_list[choice_fare]
+
+    #define filename for CSV
+    filename_company = fare_list[choice_fare]
+
+    return filename_company, fare_url
+
+
 #grab all basic informations from the website_container
-def get_buffer():
+def get_buffer(fare_url):
 
     page_number_max = 100   #increase if not all pages are scraped
     page_number = 1         #first page to start
 
-    filename = "buffer.csv"
-    b = open(filename, "w")
+    filename_buffer = "buffer.csv"
+    b = open(filename_buffer, "w")
     headers = "link; company_name"
     b.write(headers)
 
     for x in range(0, page_number_max):
-        myurl = "https://guide.swissbau.ch/de/aussteller?page=" + str(page_number)
+        myurl = fare_url + "/de/aussteller?page=" + str(page_number)
         page_number += 1
 
         #connencting to site and grabbing content
@@ -34,7 +68,7 @@ def get_buffer():
             #name of the company
             company_name = container.div.picture.img["alt"]
             #link to profile page
-            link = "https://guide.swissbau.ch" + container.div.a["href"]
+            link = fare_url + container.div.a["href"]
             b.write("\n" + link + ";" + company_name)
             print(company_name + ", " + link)
 
@@ -42,18 +76,18 @@ def get_buffer():
 
 
 #grab all detailed inforamtion from the website
-def get_details():
+def get_details(filename_company, fare_url):
 
-    filename = "buffer.csv"
-    b = open(filename, "r+")
+    filename_buffer = "buffer.csv"
+    b = open(filename_buffer, "r+")
     data = csv.reader(b, delimiter=';')
 
     #ignore header row
     headers = next(data)
 
     #write to new CSV file
-    filename = "index_exhibitors.csv"
-    f = open(filename, "w")
+    filename_company = filename_company + ".csv"
+    f = open(filename_company, "w")
     headers = "link; company_name; adress; postal; city; country; phone; website; contact_name; contact_position; contact_link"
     f.write(headers)
 
@@ -163,7 +197,7 @@ def get_details():
 
             #contact_link
             contact_link_container = container_contact[2].div.a["href"]
-            contact_link = "https://guide.swissbau.ch" + contact_link_container
+            contact_link = fare_url + contact_link_container
 
         except IndexError:
             try:
@@ -177,7 +211,7 @@ def get_details():
 
                 #contact_link
                 contact_link_container = container_contact[3].div.a["href"]
-                contact_link = "https://guide.swissbau.ch" + contact_link_container
+                contact_link = fare_url + contact_link_container
             except:
                 pass
 
@@ -189,3 +223,19 @@ def get_details():
 
     b.close()
     f.close()
+
+
+if __name__ == "__main__":
+    filename_company, fare_url = url()
+
+    print("####################################################")
+    print("Loading Buffer")
+    print("####################################################")
+
+    #get_buffer(fare_url)
+
+    print("####################################################")
+    print("Buffer finished - Loading Details")
+    print("####################################################")
+
+    get_details(filename_company, fare_url)
