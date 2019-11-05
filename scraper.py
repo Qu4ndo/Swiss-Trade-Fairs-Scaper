808059fe2d822633b04204cc1ccd523a17e9b2b0
Manuel <pfandlmanuel@protonmail.com>
2019-11-05 16:20:52 +0100

    Included the start of the details_scraper.py command

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 import csv
+import os
 
 page_number_max = 100   #increase if not all pages are scraped
 page_number = 1         #first page to start
@@ -35,3 +36,5 @@ for x in range(0, page_number_max):
         print(company_name + ", " + link)
 
 b.close()
+
+#os.system("python3 details_scraper.py"
